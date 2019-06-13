06ee9653e1dd9ebb35a693cd6b664528a84d0b43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-13 17:37:29 -0400

    first

---
+++ __snapshots__/spec.js
@@ -0,0 +1,7 @@
+exports['works 1'] = `
+foo
+`
+
+exports['works 2'] = `
+bar
+`
---
+++ spec.js
@@ -0,0 +1,6 @@
+const snapshot = require('snap-shot-it')
+
+it('works', () => {
+  snapshot('foo')
+  snapshot('bar')
+})
