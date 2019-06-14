760b91fd95a150a5668090464e4939f1b0023816
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-14 10:14:11 -0400

    enabled both

--- __snapshots__/spec.js
+++ __snapshots__/spec.js
@@ -5,3 +5,7 @@ foo
 exports['works again 1'] = 42
 
 exports['works again 2'] = 101
+
+exports['works 2'] = `
+bar
+`
--- spec.js
+++ spec.js
@@ -2,7 +2,7 @@ const snapshot = require('snap-shot-it')
 
 it('works', () => {
   snapshot('foo')
-  // snapshot('bar')
+  snapshot('bar')
 })
 
 it('works again', () => {
