64551c8ce8727ecba6a0452ccfe42fab50af13ec
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-14 10:13:33 -0400

    testing

--- __snapshots__/spec.js
+++ __snapshots__/spec.js
@@ -2,6 +2,6 @@ exports['works 1'] = `
 foo
 `
 
-exports['works 2'] = `
-bar
-`
+exports['works again 1'] = 42
+
+exports['works again 2'] = 101
--- spec.js
+++ spec.js
@@ -4,3 +4,8 @@ it('works', () => {
   snapshot('foo')
   // snapshot('bar')
 })
+
+it('works again', () => {
+  snapshot(42)
+  snapshot(101)
+})
