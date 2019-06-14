ed3d6fe4050c576901ec4c0d71c7de7cce056c1f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-14 10:16:08 -0400

    both snapshots and with sorting

--- __snapshots__/spec.js
+++ __snapshots__/spec.js
@@ -2,10 +2,10 @@ exports['works 1'] = `
 foo
 `
 
-exports['works again 1'] = 42
-
-exports['works again 2'] = 101
-
 exports['works 2'] = `
 bar
 `
+
+exports['works again 1'] = 42
+
+exports['works again 2'] = 101
